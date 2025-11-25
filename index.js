210d3a820b70330ba832fb443434bd9bb4348c2e
Raja12344j <164785371+Raja12344j@users.noreply.github.com>
2025-11-25 13:14:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {
 } = require("@whiskeysockets/baileys");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Create necessary directories
 if (!fs.existsSync("temp")) {
